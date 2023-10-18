1385c1a55ae6d1ecb9490da28bc5ecc44679c915
Tianyu Chen <sweetyfish@deepin.org>
2023-10-18 16:58:08 +0800

    feat: update node-promise to 8.3.0

--- test/extensions-tests.js
+++ test/extensions-tests.js
@@ -4,8 +4,8 @@ var sentinel = {}
 var promise = new Promise(function (resolve) {
   resolve(sentinel)
 })
-var thenable = {then: function (fullfilled, rejected) { fullfilled(sentinel) }}
-var thenableRejected = {then: function (fullfilled, rejected) { rejected(sentinel) }}
+var thenable = {then: function (fulfilled, rejected) { fulfilled(sentinel) }}
+var thenableRejected = {then: function (fulfilled, rejected) { rejected(sentinel) }}
 
 var a = {}
 var b = {}
@@ -128,6 +128,332 @@ describe('extensions', function () {
         assert(this === ctx)
         done()
       })
+    })
+  })
+  describe('Promise.any(...)', function () {
+    describe('an array', function () {
+      describe('that is empty', function () {
+        it('returns a rejected promise for an empty array', function (done) {
+          var res = Promise.any([])
+          assert(res instanceof Promise)
+          res.catch(function (err) {
+            assert(Array.isArray(err.errors))
+            assert(err.errors.length === 0)
+          }).nodeify(done)
+        })
+        it('returns a rejected promise for not argument', function (done) {
+          var res = Promise.any()
+          assert(res instanceof Promise)
+          res.catch(function (err) {
+            assert(err instanceof Error)
+          }).nodeify(done)
+        })
+      })
+      describe('of objects', function () {
+        it('resolved with a first fulfilled value', function (done) {
+          var res = Promise.any([a, b, c])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(a === res)
+          }).nodeify(done)
+        })
+      })
+      describe('of promises', function () {
+        it('resolved with a first fulfilled value', function (done) {
+          var res = Promise.any([B, C])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(b === res)
+          }).nodeify(done)
+        })
+      })
+      describe('of mixed values', function () {
+        it('returns a promise for an array containing the fulfilled values', function (done) {
+          var res = Promise.any([c,B])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(res === c)
+          }).nodeify(done)
+        })
+      })
+      describe('containing all rejected promise', function () {
+        it('rejects the resulting promise', function (done) {
+          var rejectionB ={test:2}
+          var rejectedB = new Promise(function (resolve, reject) { reject(rejectionB) })
+          var res = Promise.any([rejected, rejectedB])
+          assert(res instanceof Promise)
+          res.catch(function (err) {
+            assert(Array.isArray(err.errors))
+            assert(err.errors[0] === rejection)
+            assert(err.errors[1] === rejectionB)
+            assert(err.errors.length === 2)
+          }).nodeify(done)
+        })
+      })
+      describe('when given a foreign promise', function () {
+        it('should provide the correct value of `this`', function (done) {
+          var p = {then: function (onFulfilled) { onFulfilled({self: this}); }};
+          Promise.any([p]).then(function (results) {
+            assert(p === results.self);
+          }).nodeify(done);
+        });
+      });
+    })
+  })
+  describe('Promise.allSettled(...)', function () {
+    describe('an array', function () {
+      describe('that is empty', function () {
+        it('returns a promise for an empty array', function (done) {
+          var res = Promise.allSettled([])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res.length === 0)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('of objects', function () {
+        it('returns a promise for the array', function (done) {
+          var res = Promise.allSettled([a, b, c])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "fulfilled")
+            assert(res[1].value === b)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('of promises', function () {
+        it('returns a promise for an array containing the fulfilled values', function (done) {
+          var d = {}
+          var resolveD
+          var res = Promise.allSettled([A, B, C, new Promise(function (resolve) { resolveD = resolve })])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "fulfilled")
+            assert(res[1].value === b)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+            assert(res[3].status === "fulfilled")
+            assert(res[3].value === d)
+          })
+          .nodeify(done)
+          resolveD(d)
+        })
+      })
+      describe('of mixed values', function () {
+        it('returns a promise for an array containing the fulfilled values', function (done) {
+          var res = Promise.allSettled([A, b, C])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "fulfilled")
+            assert(res[1].value === b)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('containing at least one rejected promise', function () {
+        it('should not rejects the resulting promise', function (done) {
+          var res = Promise.allSettled([A, rejected, C])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "rejected")
+            assert(res[1].reason === rejection)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('containing at least one eventually rejected promise', function () {
+        it('rejects the resulting promise', function (done) {
+          var rejectB
+          var rejected = new Promise(function (resolve, reject) { rejectB = reject })
+          var res = Promise.allSettled([A, rejected, C])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "rejected")
+            assert(res[1].reason === rejection)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+          rejectB(rejection)
+        })
+      })
+      describe('with a promise that resolves twice', function () {
+        it('still waits for all the other promises', function (done) {
+          var a = 1;
+          var fakePromise = {then: function (onFulfilled) { onFulfilled(a); onFulfilled(2) }}
+          var eventuallyRejected = {then: function (_, onRejected) { this.onRejected = onRejected }}
+          var res = Promise.allSettled([fakePromise, eventuallyRejected])
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "rejected")
+            assert(res[1].reason === rejection)
+          })
+          .nodeify(done)
+          eventuallyRejected.onRejected(rejection);
+        })
+      })
+      describe('when given a foreign promise', function () {
+        it('should provide the correct value of `this`', function (done) {
+          var p = {then: function (onFulfilled) { onFulfilled({self: this}); }};
+          Promise.allSettled([p]).then(function (results) {
+            assert(p === results[0].value.self);
+          }).nodeify(done);
+        });
+      });
+    })
+    describe('a Set', function () {
+      describe('that is empty', function () {
+        it('returns a promise for an empty array', function (done) {
+          var res = Promise.allSettled(new Set([]))
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res.length === 0)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('of objects', function () {
+        it('returns a promise for the array', function (done) {
+          var res = Promise.allSettled(new Set([a, b, c]))
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "fulfilled")
+            assert(res[1].value === b)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('of promises', function () {
+        it('returns a promise for an array containing the fulfilled values', function (done) {
+          var d = {}
+          var resolveD
+          var res = Promise.allSettled(new Set([A, B, C, new Promise(function (resolve) { resolveD = resolve })]))
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "fulfilled")
+            assert(res[1].value === b)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+            assert(res[3].status === "fulfilled")
+            assert(res[3].value === d)
+          })
+          .nodeify(done)
+          resolveD(d)
+        })
+      })
+      describe('of mixed values', function () {
+        it('returns a promise for an array containing the fulfilled values', function (done) {
+          var res = Promise.allSettled(new Set([A, b, C]))
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "fulfilled")
+            assert(res[1].value === b)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('containing at least one rejected promise', function () {
+        it('rejects the resulting promise', function (done) {
+          var res = Promise.allSettled(new Set([A, rejected, C]))
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "rejected")
+            assert(res[1].reason === rejection)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+        })
+      })
+      describe('containing at least one eventually rejected promise', function () {
+        it('rejects the resulting promise', function (done) {
+          var rejectB
+          var rejected = new Promise(function (resolve, reject) { rejectB = reject })
+          var res = Promise.allSettled(new Set([A, rejected, C]))
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "rejected")
+            assert(res[1].reason === rejection)
+            assert(res[2].status === "fulfilled")
+            assert(res[2].value === c)
+          })
+          .nodeify(done)
+          rejectB(rejection)
+        })
+      })
+      describe('with a promise that resolves twice', function () {
+        it('still waits for all the other promises', function (done) {
+          var a = 1
+          var fakePromise = {then: function (onFulfilled) { onFulfilled(a); onFulfilled(2) }}
+          var eventuallyRejected = {then: function (_, onRejected) { this.onRejected = onRejected }}
+          var res = Promise.allSettled(new Set([fakePromise, eventuallyRejected]))
+          assert(res instanceof Promise)
+          res.then(function (res) {
+            assert(Array.isArray(res))
+            assert(res[0].status === "fulfilled")
+            assert(res[0].value === a)
+            assert(res[1].status === "rejected")
+            assert(res[1].reason === rejection)
+          })
+          .nodeify(done)
+          eventuallyRejected.onRejected(rejection);
+        })
+      })
+      describe('when given a foreign promise', function () {
+        it('should provide the correct value of `this`', function (done) {
+          var p = {then: function (onFulfilled) { onFulfilled({self: this}); }};
+          Promise.allSettled(new Set([p])).then(function (results) {
+            assert(p === results[0].value.self);
+          }).nodeify(done);
+        });
+      });
     })
   })
   describe('Promise.all(...)', function () {
